747bf0a076fa0aded5cf953d614ee5c2a80bb1d7
aGamer106 <agamer106@yahoo.com>
2025-11-28 00:36:07 +0000

    implemented HelpDeskAgent.java object

--- src/main/java/com/cms/assignment/helpdeskagent/HelpDeskAgent.java
+++ src/main/java/com/cms/assignment/helpdeskagent/HelpDeskAgent.java
@@ -1,4 +1,17 @@
 package com.cms.assignment.helpdeskagent;
 
-public class HelpDeskAgent {
+import com.cms.assignment.appuser.User;
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class HelpDeskAgent extends User {
+
+    private String employeeId;
+
+    private String department;
+
 }
