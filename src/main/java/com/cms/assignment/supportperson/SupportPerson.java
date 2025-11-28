aedff0e5299e6899329b1eba5ee140764831d821
aGamer106 <agamer106@yahoo.com>
2025-11-28 00:41:09 +0000

    SupportPerson.java implemented

--- src/main/java/com/cms/assignment/supportperson/SupportPerson.java
+++ src/main/java/com/cms/assignment/supportperson/SupportPerson.java
@@ -1,4 +1,18 @@
 package com.cms.assignment.supportperson;
 
-public class SupportPerson {
+
+import com.cms.assignment.appuser.User;
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class SupportPerson extends User {
+
+    private String expertiseArea;
+
+    private boolean isAvailable;
+
 }
