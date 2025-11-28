12b58594f0399973203a2e6100893a77143f3fa0
aGamer106 <agamer106@yahoo.com>
2025-11-28 00:42:26 +0000

    HelpDeskManager.java implemented

--- src/main/java/com/cms/assignment/helpdeskmanager/HelpDeskManager.java
+++ src/main/java/com/cms/assignment/helpdeskmanager/HelpDeskManager.java
@@ -1,4 +1,17 @@
 package com.cms.assignment.helpdeskmanager;
 
-public class HelpDeskManager {
+import com.cms.assignment.appuser.User;
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class HelpDeskManager extends User {
+
+    private String regionManaged;
+
+    private String managementLevel;
+
 }
