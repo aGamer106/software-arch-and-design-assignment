a2ab42c41b4457b39bdd1f4f4449d172e95e527e
aGamer106 <agamer106@yahoo.com>
2025-11-30 21:07:28 +0000

    implemented the creation of a complaint functionality via createComplaint() method

--- src/main/java/com/cms/assignment/complaint/ComplaintController.java
+++ src/main/java/com/cms/assignment/complaint/ComplaintController.java
@@ -1,11 +1,12 @@
 package com.cms.assignment.complaint;
 
+import com.cms.assignment.ComplaintStatus;
+import com.cms.assignment.appuser.UserRepository;
+import com.cms.assignment.consumer.Consumer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -15,9 +16,21 @@ public class ComplaintController {
     @Autowired
     private ComplaintRepository complaintRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/{consumerId}")
     public List<Complaint> getMyComplaints(@PathVariable Integer consumerId) {
         return complaintRepository.findByConsumerId(consumerId);
+    }
+
+    @PostMapping("{consumerId}")
+    public Complaint createComplaint(@PathVariable Integer consumerId, @RequestBody Complaint complaint) {
+        Consumer consumer = (Consumer) userRepository.findById(consumerId).orElseThrow(() -> new RuntimeException("Consumer not found"));
+        complaint.setConsumer(consumer);
+        complaint.setSubmissionDate(LocalDate.now());
+        complaint.setStatus(ComplaintStatus.PENDING);
+        return complaintRepository.save(complaint);
     }
 
 }
