b9a72acb4f4e74624c25a2842e86f07bc107d46f
aGamer106 <agamer106@yahoo.com>
2025-12-03 02:03:40 +0000

    implemented queries to have access to the complaints in my frontend

--- src/main/java/com/cms/assignment/complaint/ComplaintRepository.java
+++ src/main/java/com/cms/assignment/complaint/ComplaintRepository.java
@@ -1,11 +1,21 @@
 package com.cms.assignment.complaint;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ComplaintRepository extends JpaRepository<Complaint, Integer> {
 
     List<Complaint> findByConsumerId(Integer consumerId);
+
+    @Query("SELECT c FROM Complaint c WHERE c.assignedAgent.id = :agentId")
+    List<Complaint> findByAssignedAgentId(Integer agentId);
+    @Query("SELECT c FROM Complaint c WHERE c.assignedAgent.id = :agentId AND c.status != 'SOLVED'")
+    List<Complaint> findActiveByAgentId(Integer agentId);
+
+    @Query(value = "SELECT * FROM complaint WHERE agent_id IS NULL AND status = 'PENDING' ORDER BY submission_date ASC LIMIT 1", nativeQuery = true)
+    Optional<Complaint> findFirstUnassigned();
 
 }
